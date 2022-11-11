6f5d0f58d88bee580ef9edd80cf5df57698c8d8c
Tathyanna Maximiano <tathyanna.maximiano@gmail.com>
2022-11-10 21:53:10 -0500

    searchbar

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import config from '../config.json';
 import styled from 'styled-components';
 import { CSSReset } from '../src/components/CSSReset';
@@ -6,6 +7,7 @@ import Menu from '../src/components/Menu';
 
 function HomePage() {
 	// const homePageStyle = { backgroundColor: 'red' };
+	const [searchValue, setSearchValue] = React.useState(' ');
 	return (
 		<>
 			<CSSReset />
@@ -16,9 +18,11 @@ function HomePage() {
 					flex: 1,
 				}}
 			>
-				<Menu />
+				<Menu searchValue={searchValue} setSearchValue={setSearchValue} />
 				<Header />
-				<Timeline playlists={config.playlists}>Content</Timeline>
+				<Timeline searchValue={searchValue} playlists={config.playlists}>
+					Content
+				</Timeline>
 			</div>
 		</>
 	);
@@ -42,15 +46,27 @@ const StyledHeader = styled.div`
 		width: 100%;
 		padding: 1rem 32rem;
 		gap: 1rem;
-		margin-top: 3.5rem;
 		margin-left: -30rem;
 		/* background-image: url('../src/components/assets/alexander-shatov-niUkImZcSP8-unsplash (1).jpg'); */
 	}
 `;
 
+const StyledBanner = styled.div`
+	background: rgb(2, 0, 36);
+	background: linear-gradient(
+		90deg,
+		rgba(2, 0, 36, 1) 0%,
+		rgba(9, 70, 121, 1) 41%,
+		rgba(0, 212, 255, 1) 100%
+	);
+	/* background-image: url(${config.bg}); */
+	height: 14.5rem;
+`;
+
 function Header() {
 	return (
 		<StyledHeader>
+			<StyledBanner />
 			{/* <img src="banner" /> */}
 			<section className="user-info">
 				<img src={`https://github.com/${config.github}.png`} />
@@ -63,26 +79,34 @@ function Header() {
 	);
 }
 
-function Timeline(props) {
+function Timeline({ searchValue, ...props }) {
 	const playlistNames = Object.keys(props.playlists);
 	return (
 		<StyledTimeline>
 			{playlistNames.map((playlistName) => {
 				const videos = props.playlists[playlistName];
-				console.log(playlistName);
-				console.log(videos);
+				{
+					/* console.log(playlistName);
+				console.log(videos); */
+				}
 				return (
 					<section>
 						<h2>{playlistName}</h2>
 						<div>
-							{videos.map((video) => {
-								return (
-									<a href={video.url}>
-										<img src={video.thumb} />
-										<span>{video.title}</span>
-									</a>
-								);
-							})}
+							{videos
+								.filter((video) => {
+									const titleNormalized = video.title.toLowerCase();
+									const searchValueNormalized = searchValue.toLowerCase();
+									return titleNormalized.includes(searchValueNormalized);
+								})
+								.map((video) => {
+									return (
+										<a href={video.url}>
+											<img src={video.thumb} />
+											<span>{video.title}</span>
+										</a>
+									);
+								})}
 						</div>
 					</section>
 				);
